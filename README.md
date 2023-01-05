77b690838d2300aa309916842bd2c903a3c8e857
Dhiaa Shalabi <dhiaa.shalabi@gmail.com>
2023-01-05 19:40:37 +0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,12 @@
+# JS - If statements
+
+## Overview
+Write a function that fulfills the following requirements:
+
+## Instructions
+1. It takes a single parameter, a number.
+2. If the number is between 10 and 20, console.log the number.
+3. If the number is less than 100, return the number divided by two.
+4. If the the number is less than 10, console.log the square of the number.
+5. If the number is greater or equal to 100 but less than 200, return just the number.
+6. Otherwise return the number multiplied by two.
---
+++ script.js
@@ -0,0 +1,17 @@
+function numberFunction(number) {
+    if (number >= 10 && number <= 20) {
+        console.log(number);
+    } else if (number < 100) {
+        return number / 2;
+    } else if (number < 10) {
+        console.log(number * number);
+    } else if (number >= 100 && number < 200) {
+        return number;
+    } else {
+        return number * 2;
+    }
+}
+
+document.getElementById("btn").addEventListener("click", function() {
+    document.getElementById("result").innerHTML = numberFunction(document.getElementById("number").value);
+});
